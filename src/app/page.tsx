249c482e1f897774116edd6e4fbb7206fa497300
tomi <tomi@tomi-2.local>
2025-04-09 19:42:45 +0900

    更新 Docker 配置和环境变量

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { FC } from 'react';
+
+const Home: FC = () => {
   return (
     <div>
       <h1>欢迎使用智能简历系统</h1>
     </div>
   );
-} 
+};
+
+export default Home; 
